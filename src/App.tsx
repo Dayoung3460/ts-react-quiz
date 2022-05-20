d5cd4f34ad795d2d6cc57a7d63484a85a13a2a3b
김다영 (클라라) <kimdayoung771@gmail.com>
2022-05-20 19:27:02 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import QuestionCard from "./components/QuestionCard";
+
+function App() {
+    const startTrivia = async () => {
+
+    }
+
+    const checkAnswers = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+    }
+
+    const nextQuestion = () => {
+
+    }
+
+  return (
+    <div className="App">
+        <h1>React Quiz</h1>
+        <button className="start" onClick={startTrivia}>
+            Start
+        </button>
+        <p className="score">Score: </p>
+        <p>Loading Questions...</p>
+        <QuestionCard/>
+        <button className="next" onClick={nextQuestion}>Next Question</button>
+    </div>
+  );
+}
+
+export default App;
