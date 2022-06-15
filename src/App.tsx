5a24e4f66f8c960117a40e38bc79c42ca784b36f
dayoung <kimdayoung771@gmail.com>
2022-06-15 21:10:41 +0900

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
     <>
       <GlobalStyle/>
       <Wrapper className="App">
-      <h1>React Quiz</h1>
+      <h1>QUIZ</h1>
       { gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
         <button className="start" onClick={startTrivia}>Start</button>
       ) : null}
--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 export const Wrapper = styled.div`
   max-width: 1100px;
-  background: #ebfeff;
+  background: #fff;
   border-radius: 10px;
-  border: 2px solid #0085a3;
+  border: 2px solid var(--color-dark-purple);
   padding: 20px;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.25);
   text-align: center;
